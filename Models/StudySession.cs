5ea76bc85420e2842667b4372228fc71dc8e4128
ahmad <ahmad2315753@gmail.com>
2026-03-16 03:17:46 +0300

    Update StudySession file

--- Models/StudySession.cs
+++ Models/StudySession.cs
@@ -1,12 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+
 namespace StudySessionAPI.Models
 {
     public class StudySession
     {
         public int Id {get; set;}
+        [Required]
+        [MinLength(3)]
         public required string Subject {get; set;}
         public DateTime StartTime {get; set;}
         public DateTime EndTime {get; set;}
-        public int FocusLevel {get; set;} // 1 - 5
-        public string? Notes {get; set;}
+        [Required]
+        [Range(1,5)]
+        public int FocusLevel {get; set;}
+        [AllowNull]
+        public string Notes {get; set;} = "No Thing.";
     }
 }
